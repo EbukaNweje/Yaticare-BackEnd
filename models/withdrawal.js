c5ec796e2ebbdffdaff2a328801a32c63861c220
ColdDdev <colindecorce@gmail.com>
2025-09-21 16:03:51 +0100

    D

---
+++ models/withdrawal.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const WithdrawalSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    method: {
+      type: String,
+      required: true,
+    },
+    walletAddress: {
+      type: String,
+    },
+    bankName: {
+      type: String,
+    },
+    accountNumber: {
+      type: String,
+    },
+    accountName: {
+      type: String,
+    },
+    processedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("withdrawal", WithdrawalSchema);
