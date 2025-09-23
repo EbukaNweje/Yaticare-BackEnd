185b7b5bfdd20f6342c0a9a204957de128a829d1
ColdDdev <colindecorce@gmail.com>
2025-09-23 11:19:23 +0100

    D

--- models/Deposit.js
+++ models/Deposit.js
@@ -1,40 +1,55 @@
 const mongoose = require("mongoose");
-const { DateTime } = require("luxon");
 
-const depositSchema = new mongoose.Schema({
-  transactionType: {
-    type: String,
-    default: "Deposit",
+const depositSchema = new mongoose.Schema(
+  {
+    transactionType: {
+      type: String,
+      default: "Deposit",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    amountusdt: {
+      type: Number,
+      required: true,
+    },
+    PaymentType: {
+      type: String,
+      enum: ["USDT", "BANK"],
+    },
+    total: {
+      type: Number,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed"],
+      default: "pending",
+    },
+    depositDate: {
+      type: Date,
+      default: () => new Date(),
+      get: (val) =>
+        val
+          ? new Date(val).toLocaleString("en-GB", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+          : null,
+    },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  amountusdt: {
-    type: Number,
-    required: true,
-  },
-  PaymentType: {
-    type: String,
-    enum: ["USDT", "BANK"],
-  },
-  total: {
-    type: Number,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "confirmed"],
-    default: "pending",
-  },
-  depositDate: {
-    type: String,
-    default: () => Date.now().toLocaleString(),
-  },
-});
+  {
+    toJSON: { getters: true },
+    toObject: { getters: true },
+  }
+);
 
 const depositModel = mongoose.model("deposit", depositSchema);
 
