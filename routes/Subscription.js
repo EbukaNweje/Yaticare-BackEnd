03b636cf32635122383b67c3b1b2e1d90d9642b6
ColdDdev <colindecorce@gmail.com>
2026-01-16 08:30:02 +0100

    Df

--- routes/Subscription.js
+++ routes/Subscription.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 router.post("/userSubcription", subscriptions.createSubscription);
 router.get("/getusrSubcription/:userId", subscriptions.getUserSubscriptions);
 router.patch(
-  "/recycleSubscription/:recycleSubscription",
+  "/recycleSubscription/:subscriptionId",
   subscriptions.recycleSubscription
 );
 // router.get("/subscription/:userId", subscriptions.getOneSubscription);
