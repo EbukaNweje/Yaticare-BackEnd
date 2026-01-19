c727db5d47f9d4b3830c0b91457266aa8661ace0
ColdDdev <colindecorce@gmail.com>
2026-01-19 12:06:33 +0100

    D

--- routes/History.js
+++ routes/History.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { getoneUserHistory, depositHistory } = require("../controllers/History");
 
-router.get("/getallhistory/:id", getoneUserHistory);
+router.get("/getallhistory/:userId", getoneUserHistory);
 router.get("/deposithistory/:id", depositHistory);
 
 module.exports = router;
