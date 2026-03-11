337bb1b4cff614602152d070199075437906a5d8
ColdDdev <colindecorce@gmail.com>
2026-03-11 15:01:58 +0100

    D

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,12 @@ const middleware = require("../middleware/Middleware");
 const router = require("express").Router();
 
 router.post("/register", AuthController.register);
-router.post("/login",middleware.isLogin, AuthController.login);
+router.post("/login", middleware.isLogin, AuthController.login);
 router.post("/logout/:id", AuthController.logout);
 router.post("/forgot-password", AuthController.forgetPassword);
-router.post("/reset-password/:id", AuthController.resetPassword);
+router.put("/reset-password/:id", AuthController.resetPassword);
 router.post("/change-password/:id", AuthController.changePassword);
 router.post("/create-pin/:id", AuthController.creeatepin);
 router.post("/change-pin/:id", AuthController.changepin);
-
 
 module.exports = router;
